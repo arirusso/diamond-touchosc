165c4426dc1e6e0bc44f1f0c165034c62c1d2e50
Ari Russo <ari.russo@gmail.com>
2011-10-25 21:33:27 -0400

    ipod version working

--- iphone-ipod/one_instance_with_keyboard.rb
+++ iphone-ipod/one_instance_with_keyboard.rb
@@ -9,20 +9,35 @@ require "diamond-touchosc"
 @output = UniMIDI::Output.gets
 
 arpeggiator_osc_controls = {
-  "/1/fader1" => { 
+  "/1/rotary1" => { 
+    :translate => 30..230,
+    :action => Proc.new { |arpeggiator, val| arpeggiator.tempo = val }
+  },
+  "/1/multifader1/1" => {
+    :translate => [128, 64, 32, 24, 16, 12, 8, 4, 2, 1],
+    :action => Proc.new { |arpeggiator, val| arpeggiator.rate = val }
+  },
+  "/1/multifader1/2" => {
+    :translate => 1..200,
+    :action => Proc.new { |arpeggiator, val| arpeggiator.gate = val }
+  },
+  "/1/multifader1/3" => {
+    :translate => 0..7,
+    :action => Proc.new { |arpeggiator, val| arpeggiator.range = val }
+  },
+  "/1/multifader1/4" => {
     :translate => -24..24,
     :action => Proc.new { |arpeggiator, val| arpeggiator.interval = val }
   },
-  "/1/fader2" => { 
+  "/1/multifader1/5" => {
+    :translate => -10..10,
+    :action => Proc.new { |arpeggiator, val| arpeggiator.pattern_offset = val }
+  },
+  "/1/multifader1/6" => {
     :translate => -24..24,
-    :action => Proc.new { |arpeggiator, val| arpeggiator.transpose = val }
-  }
+    :action => Proc.new { |arpeggiator, val| p val; arpeggiator.transpose = val }
+  },
 }
-
-keyboard = TouchOSC::Keyboard.new do |note_num|
-  note = MIDIMessage::NoteOn.new(note_num - 12, 0, 100)
-  arp.add(note)
-end
 
 arpeggiator_opts = { 
   :gate => 90,   
@@ -33,11 +48,20 @@ arpeggiator_opts = {
   :rate => 8,
   :resolution => 128,
   :osc_map => arpeggiator_osc_controls,
-  :osc_input_port => 8000
+  :osc_input_port => 8000,
+  :osc_output => { :host => "192.168.1.5", :port => 9000 }
 }
 
 arp = Diamond::Arpeggiator.new(110, arpeggiator_opts)
 
+arp.osc_send("/1/rotary1", (arp.tempo / 1000))
+
+keyboard = TouchOSC::Keyboard.new do |pressure, note_num|
+  note = MIDIMessage::NoteOn.new(0, note_num, 100)
+  action = pressure > 0 ? :add : :remove
+  arp.send(action, note)
+end
+
 keyboard.osc_start(:input_port => 8000)
-   
+
 arp.start(:focus => true)
--- lib/controllers/keyboard.rb
+++ lib/controllers/keyboard.rb
@@ -2,37 +2,46 @@ module TouchOSC
   
   class Keyboard
     
-    includes OSCAccessible
+    include OSCAccessible
     
     attr_reader :connected, :octave
     
     DefaultOctave = 1
     
-    osc_receive "/1/hold" { |keyboard, val| keyboard.hold(val == 1) }
-    osc_receive "/1/octave_down" { |keyboard, val| keyboard.octave -= 1 }
-    osc_receive "/1/octave_up" { |keyboard, val| keyboard.octave += 1 }
-    
     def initialize(options = {}, &block)
       @octave = options[:octave] || DefaultOctave
       @maxhold = options[:max_hold] || 6
       @hold = false
-      
-      #@connected = [connected].flatten.compact
+      @id = options[:id] || 1
+
+      initialize_controls
       initialize_keys(&block)
     end
     
     def octave=(val)
-      @octave = val if val >= -1 && val <= 5
+      @octave = val if val >= 0 && val <= 5
+    end
+    
+    def hold(active)
+      @hold = active
     end
     
     private
     
+    def initialize_controls
+      osc_receive("/#{@id}/kb/toggle1") { |keyboard, val| keyboard.hold(val == 1) }
+      osc_receive("/#{@id}/kb/push1") { |keyboard, val| keyboard.octave -= 1 }
+      osc_receive("/#{@id}/kb/push2") { |keyboard, val| keyboard.octave += 1 }
+    end
+    
     def initialize_keys(&block)
-      osc_receive "/1/key" do |keyboard, val| 
-        keyboard.connected.each do |inst|
-          scale_degree = val * 100
-          note_num = scale_degree + (12 * (keyboard.octave + 1))
-          yield(note_num)
+      regex = /\/#{@id}\/kb\/key(\d+)/
+      osc_receive(regex) do |keyboard, val, msg| 
+        match = msg.address.scan(regex).flatten
+        unless match.empty?
+          scale_degree = (match.first.to_i - 1)
+          note_num = scale_degree + (12 * keyboard.octave)
+          yield(val, note_num.to_i)
         end
       end
     end
