28cb53abbd16197fd1b98a263bc21055b1bccf30
Ari Russo <ari.russo@gmail.com>
2011-11-01 23:28:24 -0400

    new osc-access api updates

--- iphone-ipod/arp_with_kbd.rb
+++ iphone-ipod/arp_with_kbd.rb
@@ -1,6 +1,5 @@
 #!/usr/bin/env ruby
 $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
-
 #
 
 require "diamond"
@@ -9,15 +8,23 @@ require "diamond-touchosc"
 @output = UniMIDI::Output.gets
 
 arpeggiator_osc_controls = {
-  "/1/toggle1" => { 
-    :action => Proc.new { |arpeggiator, val| arpeggiator.toggle_start }
+  "/1/toggle1" => {
+    :initialize => :running,
+    :translate => :boolean
+  },
+  "/1/toggle2" => {
+    :accessor => :mute,
+    :translate => :boolean
+  },
+  "/1/push1" => {
+    :accessor => :reset
   },
   "/1/rotary1" => { 
-    :accessor => :tempo
+    :accessor => :tempo,
     :translate => 30..230
   },
   "/1/multifader1/1" => {
-    :accessor => :rate
+    :accessor => :rate,
     :translate => [128, 64, 32, 24, 16, 12, 8, 4, 2, 1]
   },
   "/1/multifader1/2" => {
@@ -25,7 +32,7 @@ arpeggiator_osc_controls = {
     :translate => 1..200
   },
   "/1/multifader1/3" => {
-    :accessor => :range
+    :accessor => :range,
     :translate => 0..7
   },
   "/1/multifader1/4" => {
@@ -52,18 +59,18 @@ arpeggiator_opts = {
   :resolution => 128,
   :osc_map => arpeggiator_osc_controls,
   :osc_input_port => 8000,
-  :osc_output => { :host => "192.168.1.8", :port => 9000 },
+  :osc_output => { :host => "192.168.1.6", :port => 9000 },
   :name => "Diamond Arpeggiator"
 }
 
 arp = Diamond::Arpeggiator.new(110, arpeggiator_opts)
-
-arp.osc_send("/1/rotary1", (arp.tempo / 1000))
 
 keyboard = TouchOSC::Keyboard.new do |pressure, note_num|
   note = MIDIMessage::NoteOn.new(0, note_num, 100)
   action = pressure > 0 ? :add : :remove
   arp.send(action, note)
 end
+
+arp.start
 
 keyboard.osc_start(:input_port => 8000).join
--- lib/controllers/keyboard.rb
+++ lib/controllers/keyboard.rb
@@ -23,25 +23,40 @@ module TouchOSC
       @octave = val if val >= 0 && val <= 5
     end
     
-    def hold(active)
+    def hold=(active)
       if !active
         @holding.each { |note_num| @on_play.call(0, note_num) } unless @holding.nil?
         @holding = nil
       else
         @holding = []
       end
+      !@holding.nil?      
     end
     
     def hold?
       !@holding.nil?
     end
+    alias_method :hold, :hold?
+    
+    protected
+    
+    def update_octave_label
+      osc_send(:octave)
+    end
     
     private
     
     def initialize_controls
-      osc_receive("/#{@id}/kb/toggle1") { |keyboard, val| keyboard.hold(val == 1) }
-      osc_receive("/#{@id}/kb/push1") { |keyboard, val| keyboard.octave -= 1 }
-      osc_receive("/#{@id}/kb/push2") { |keyboard, val| keyboard.octave += 1 }
+      osc_receive("/#{@id}/kb/toggle1", :accessor => :hold)
+      osc_receive("/#{@id}/kb/label3", :initialize => :octave)
+      osc_receive("/#{@id}/kb/push1") do |keyboard, val| 
+        keyboard.octave -= 1 if val > 0
+        keyboard.send(:update_octave_label)
+      end
+      osc_receive("/#{@id}/kb/push2") do |keyboard, val| 
+        keyboard.octave += 1 if val > 0
+        keyboard.send(:update_octave_label)
+      end
     end
     
     def initialize_keys(&block)
